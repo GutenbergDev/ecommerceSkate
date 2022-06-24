fa9fa8ee753030bee19d5293b230873ec61ec054
Gutemberg Medeiros <gutenbergdevm@hotmail.com>
2022-06-24 19:51:19 -0300

    Att produto

--- src/Components/Pages/Produto.js
+++ src/Components/Pages/Produto.js
@@ -37,31 +37,40 @@ const Produto = () => {
         <h1><img src={produto.image} alt='Imagem produto' /></h1>
       </div>
       <div className={styles.content2}>
-        {produto.discount <= 20 ? 
-          <div style={{ display: "flex", justifyContent: "center", backgroundColor: "#CCC500", width: "4rem", padding: ".2rem", fontSize: "1rem", color: "#fff"}}>
-            <span >{produto.discount}%</span> 
-          </div>
-          : produto.discount <= 40 ?
-          <div style={{display: "flex", justifyContent: "center", backgroundColor: "#FFAE00", width: "4rem", padding: ".2rem", fontSize: "1rem", color: "#fff" }}>
-            <span>{produto.discount}%</span> 
-          </div>
-          : produto.discount > 50 ?
-          <div style={{display: "flex", justifyContent: "center", backgroundColor: "#FF0000", width: "4rem", padding: ".2rem", fontSize: "1rem", color: "#fff" }}>
-            <span>{produto.discount}%</span> 
-          </div>
-          :
-          <div style={{display: "flex", justifyContent: "center", backgroundColor: "#FF0000", width: "4rem", padding: ".2rem", fontSize: "1rem", color: "#fff" }}>
-            <span>{produto.discount}%</span>
-          </div>
-        }
-        <h2>{produto.name}</h2>
-        <p className={styles.oldPrice}>de R$ {produto.OldPrice.toFixed(2)}</p>
-        <h3 className={`${styles.precoAtual}`}>por R$ {produto.price.toFixed(2)}</h3>
-        
-        <span className={styles.vista}>R$ {produto.price.toFixed(2) - ((produto.price.toFixed(2)) * (produto.discount / 100)).toFixed(2)} à vista</span>
-        <span className={styles.parcelas}>ou 10x sem juros de R$ {(produto.price / 10).toFixed(2)}</span>
-        <span style={{ fontWeight: "bold", fontSize: "1.3rem", marginTop: "1rem" }}>à vista com {produto.discount}% de desconto no boleto</span>
+        <div className={styles.pricesContainer}>
+          {produto.discount <= 20 ? 
+            <div style={{ display: "flex", justifyContent: "center", backgroundColor: "#CCC500", width: "4rem", padding: ".2rem", fontSize: "1rem", color: "#fff"}}>
+              <span >{produto.discount}%</span> 
+            </div>
+            : produto.discount <= 40 ?
+            <div style={{display: "flex", justifyContent: "center", backgroundColor: "#FFAE00", width: "4rem", padding: ".2rem", fontSize: "1rem", color: "#fff" }}>
+              <span>{produto.discount}%</span> 
+            </div>
+            : produto.discount > 50 ?
+            <div style={{display: "flex", justifyContent: "center", backgroundColor: "#FF0000", width: "4rem", padding: ".2rem", fontSize: "1rem", color: "#fff" }}>
+              <span>{produto.discount}%</span> 
+            </div>
+            :
+            <div style={{display: "flex", justifyContent: "center", backgroundColor: "#FF0000", width: "4rem", padding: ".2rem", fontSize: "1rem", color: "#fff" }}>
+              <span>{produto.discount}%</span>
+            </div>
+          }
+          <h2>{produto.name}</h2>
+          <p className={styles.oldPrice}>de R$ {produto.OldPrice.toFixed(2)}</p>
+          <h3 className={`${styles.precoAtual}`}>por R$ {produto.price.toFixed(2)}</h3>
+          
+          <span className={styles.vista}>R$ {produto.price.toFixed(2) - ((produto.price.toFixed(2)) * (produto.discount / 100)).toFixed(2)} à vista</span>
+          <span className={styles.parcelas}>ou 10x sem juros de R$ {(produto.price / 10).toFixed(2)}</span>
+          <span style={{ fontWeight: "bold", fontSize: "1.3rem", marginTop: "1rem" }}>à vista com {produto.discount}% de desconto no boleto</span>
+        </div>
+
+        <div className={styles.sizePro}>
+          <p>Tamanho: {produto.sizes}
+
+          </p>
+        </div>
       </div>
+      
     </section>
   )
 }
